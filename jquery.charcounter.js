9a66dd63ecf1e87db45bf84f7793922e6f542b07
MilkmanMedia <daniel.kontos@milkmanmedia.de>
2013-12-05 11:44:55 +0100

    Update jquery.charcounter.js
    Changed the setter for the charlength. So now the maximum charlength has to be set via the "maxlength" attribute in the input field instead of using the "rel" attribute.

--- jquery.charcounter.js
+++ jquery.charcounter.js
@@ -9,8 +9,8 @@
  * 
  * usage:
  *
- * HTML markup (Plugin uses attribute 'rel' for maximum allowed characters):
- * <input type="text" name="myTextfield" class="CharCounter" rel="100">
+ * HTML markup (Plugin uses attribute 'maxlength' for maximum allowed characters):
+ * <input type="text" name="myTextfield" class="CharCounter" maxlength="100">
  *                   
  * JavaScript init:     
  * $('.CharCounter').CharCounter(); // inits CharCounter on all elements with class 'CharCounter'
@@ -23,7 +23,7 @@
  *  
  * generated markup:
  * <div class="wrap-CharCounter">
- *   <input type="text" rel="100" class="CharCounter" name="myTextfield">
+ *   <input type="text" maxlength="100" class="CharCounter" name="myTextfield">
  *   <code style="display: block; opacity: 0.5;">100</code>
  * </div>              
  */
@@ -37,27 +37,33 @@
 
       $.extend(conf, params);
       return $(this).each(function(){            
-        var textArea = $(this).wrap('<div class="wrap-CharCounter" />');
-        var counter = $('<code/>').css({
-          display: 'block',
-          opacity: 0.5
-        }).insertAfter(textArea);
+        var el = this,
+            $el = $(this),
+            textArea = $el.wrap('<div class="wrap-CharCounter" />'),
+            counter = $('<code/>').css({
+              display: 'block',
+              opacity: 0.5
+            }).insertAfter(textArea),
+            charLength = (parseInt(textArea.attr('maxlength')));
       
         if(conf.debug){
           console.log(counter);
           console.log(textArea);
         }
-
-        counter.text((parseInt(textArea.attr('rel'))-textArea.val().length));
-        textArea.keyup(function(){          
-          if($(this).val().length <= parseInt($(this).attr('rel'))){
-           counter.text((parseInt($(this).attr('rel'))-$(this).val().length));
+        
+        
+        counter.text(charLength-textArea.val().length);
+        textArea.keyup(function(){
+          var value = $(this).val(),
+              valueLength = value.length;          
+          if(valueLength <= charLength){
+           counter.text(charLength-valueLength);
           }
-          if($(this).val().length > parseInt($(this).attr('rel'))){
-            $(this).val($(this).val().slice(0, $(this).attr('rel')));
+          if(valueLength > charLength){
+            $(this).val(value.slice(0, charLength));
           }    
           if(conf.debug){      
-            console.log('chars in ' + $(this).index(this) + ': ' + $(this).val().length);
+            console.log('chars in ' + $(this).index(this) + ': ' + valueLength);
             console.log('box height: '+ $(this).height());
           }
         });
